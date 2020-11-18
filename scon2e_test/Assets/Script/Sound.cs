e7bbc81cdb1de0e3a48b23b9caf9939de3ac1545
masa190 <masato-toguchi-19f@stu.kbc.ac.jp>
2020-11-18 11:22:39 +0900

    add Sound

---
+++ scon2e_test/Assets/Script/Sound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    private GameObject player;
+    private AudioSource audioSE;
+    public AudioClip sound01;
+    private bool SearchSEflag = true;
+    void Start()
+    {
+        audioSE = gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SearchSEflag == true)
+        {
+
+            audioSE.PlayOneShot(sound01);
+            SearchSEflag = false;
+        }
+    }
+}
