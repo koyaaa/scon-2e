5cf383022bd6782b3e88fa5686d84ff18344ed26
koyaaa <koya-uchima-19f@stu.kbc.ac.jp>
2020-11-25 14:19:04 +0900

    protoステージScene

--- scon2e_test/Assets/Script/GameOver.cs
+++ scon2e_test/Assets/Script/GameOver.cs
@@ -5,18 +5,32 @@ public class GameOver : MonoBehaviour
 {
     GameManager gameManager;
     public OnSearchView onSearch;   //OnSearchViewスクリプトを使用する
+    public float ray_distance = 0.5f;
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
-    //プレイヤーが当たり判定に入った時の処理
-    void OnTriggerEnter(Collider CapsuleCollider)
+    void Update()
     {
-        if (CapsuleCollider.gameObject.tag == "Enemy")
+        onSearch = this.GetComponent<OnSearchView>();
+        if (onSearch.WANING == true)
         {
-            gameManager.GameOver();
+            //Rayの発射地点の座標と発射する方向の設定
+            Ray ray = new Ray(this.transform.position, this.transform.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, ray_distance))
+            {
+                //ヒットしたものがプレイヤー
+                if (hit.collider.tag == "Player")
+                {
+                    gameManager.GameOver();
+                }
+            }
+            Debug.DrawRay(ray.origin, ray.direction * ray_distance, Color.red, 5);
         }
+
     }
 }
