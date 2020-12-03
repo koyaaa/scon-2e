44aa37b68d6a4ac0b5cd0ca4df16374c91c8bcb0
takayasu-masashi <65996541+takayasu-masashi@users.noreply.github.com>
2020-12-03 16:31:50 +0900

    Add files via upload

--- scon2e_test/Assets/Script/GameManager.cs
+++ scon2e_test/Assets/Script/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
@@ -8,14 +9,15 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
     //最初の処理
     void Start()
     {
+
     }
     //毎フレームの処理
     void Update()
     {
-
     }
 
     //ゲームオーバー処理
@@ -26,10 +28,17 @@ public class GameManager : MonoBehaviour
 
     public void GameClear()
     {
-    }
-
-    public void Next()
-    {
-
+        if (SceneManager.GetActiveScene().name == "ProtStage")
+        {
+            SceneManager.LoadScene("GameClear");
+        }
+        else if (SceneManager.GetActiveScene().name == "Stage2")
+        {
+            SceneManager.LoadScene("GameClear2");
+        }
+        else if(SceneManager.GetActiveScene().name == "Stage3")
+        {
+            SceneManager.LoadScene("GameClear3");
+        }
     }
 }
