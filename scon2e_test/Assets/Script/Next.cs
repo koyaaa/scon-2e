2267319f146b52f4ba18f496ff9fae1fa3194d69
takayasu-masashi <65996541+takayasu-masashi@users.noreply.github.com>
2020-12-03 16:19:32 +0900

    Add files via upload

---
+++ scon2e_test/Assets/Script/Next.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Next : MonoBehaviour
+{
+    public void OnStartButtonClicked()
+    {
+        if (SceneManager.GetActiveScene().name == "GameClear")
+        {
+            SceneManager.LoadScene("Stage2");
+        }
+        else if (SceneManager.GetActiveScene().name == "GameClear2")
+        {
+            SceneManager.LoadScene("Stage3");
+        }
+        else if (SceneManager.GetActiveScene().name == "GameClear3")
+        {
+            SceneManager.LoadScene("Stage4");
+        }
+    }
+}
