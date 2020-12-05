029acace9e1cc094be743436c313e070b3ad6d43
kaneshihiroaki <65996575+kaneshihiroaki@users.noreply.github.com>
2020-12-05 23:37:46 +0900

    Add files via upload
    箱の途中のスクリプト

---
+++ scon2e_test/Assets/Script/ChestController.cs
@@ -0,0 +1,42 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class ChestController : MonoBehaviour
+//{
+//    public Animator animator;
+//    public PlayerController playerController;   //PlayerControllerScriptスクリプトを使用する
+
+//    public bool Close = false;
+//    public float settime = 1.0f;
+//    private float CloseTime;
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (playerController.hide == true)
+//        {
+//            animator.SetBool("Open", true);
+//            Close = true;
+//        }
+
+//        if (Close == true)
+//        {
+//            CloseTime = Time.time;
+//            Debug.Log("Set");
+//            Close = false;
+//        }
+
+//        if (settime < Time.time - CloseTime)
+//        {
+//            animator.SetBool("Open", false);
+//            Debug.Log("Close");
+//        }
+//    }
+//}
