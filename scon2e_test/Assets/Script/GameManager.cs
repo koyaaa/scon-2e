7b123233aca3de409ebd30650aa804f1fac338b2
takayasu-masashi <65996541+takayasu-masashi@users.noreply.github.com>
2020-12-09 12:09:29 +0900

    ステージ選択とゲームオーバー

--- scon2e_test/Assets/Script/GameManager.cs
+++ scon2e_test/Assets/Script/GameManager.cs
@@ -12,11 +12,22 @@ public class GameManager : MonoBehaviour
     // クリア回数
     // staticにすることで新しいシーンを読み込んだ時でも削除されなくなる
     public static int CC = 0;
-
+    public static int GC = 1;
     //最初の処理
     void Start()
     {
-
+        if (SceneManager.GetActiveScene().name == "Tutorial Stage")
+        {
+            GC = 1;
+        }
+        else if (SceneManager.GetActiveScene().name == "Stage2") 
+        {
+            GC = 2;
+        }
+        else if (SceneManager.GetActiveScene().name == "Stage3")
+        {
+            GC = 3;
+        }
     }
     //毎フレームの処理
     void Update()
@@ -31,7 +42,7 @@ public class GameManager : MonoBehaviour
 
     public void GameClear()
     {
-        if (SceneManager.GetActiveScene().name == "ProtoStage")
+        if (SceneManager.GetActiveScene().name == "Tutorial Stage")
         {
             if (CC >= 0)
             {
---
+++ scon2e_test/Assets/Script/Retry.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Retry : MonoBehaviour
+{
+    int count = GameManager.GC;
+    public void OnStartButtonClicked()
+    {
+        if (count == 1)
+        {
+            SceneManager.LoadScene("Tutorial Stage");
+        }
+        else if (count == 2)
+        {
+            SceneManager.LoadScene("Stage2");
+        }
+        else if (count == 3)
+        {
+            SceneManager.LoadScene("Stage3");
+        }
+    }
+}
